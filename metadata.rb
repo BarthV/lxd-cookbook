08d233cf6f64274add511fe3d0b6d576eab552eb
Barthelemy Vessemont <bvessemont@gmail.com>
2015-03-03 16:00:33 +0100

    0.2.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bvessemont@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures lxd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version 'v0.1.9'
+version 'v0.2.0'
 supports 'ubuntu', '>= 14.04'
 
 depends 'apt'
